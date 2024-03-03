61ff1b39474f5132f61ff6fb4b9abdc5a1082e28
Pradeep Sai Uppula <pradeep@Pradeeps-MacBook-Pro.local>
2024-03-02 21:56:28 -0800

    initial wire frame of task progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
+import taskList from './data/taskList.json'
+import Card from './components/Card.jsx'
+import Tasks from './components/Tasks.jsx';
+import CreateEntry from './components/CreateEntry.jsx'
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [allTasks, setAllTasks] = useState([])
+
+    useEffect(() => {
+        setAllTasks(taskList?.tasks)
+
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> hello</h1>
-      </header>
+      <main className='taskView'>
+        <CreateEntry setAllTasks={setAllTasks}/>
+        <Tasks allTasks={allTasks}/>
+      </main>
     </div>
   );
 }
